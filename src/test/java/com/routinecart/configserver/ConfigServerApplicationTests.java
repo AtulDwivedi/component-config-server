286046125bc7b7435c94aaba11750c0639c3997e
Atul Dwivedi <atul16dwivedi@gmail.com>
2019-03-14 08:32:18 +0530

    refactored for routinecart

--- src/test/java/com/routinecart/configserver/ConfigServerApplicationTests.java
+++ src/test/java/com/routinecart/configserver/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.quickcart.configserver.componentconfigserver;
+package com.routinecart.configserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ComponentConfigServerApplicationTests {
+public class ConfigServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
